924f6af9ee6ea0f0ba2b3f7e7dac5816f329da25
Liya408 <liyabn80@gmail.com>
2024-10-01 15:12:43 +0530

    Add files via upload

---
+++ student details.py
@@ -0,0 +1,7 @@
+name=input("enter the student name")
+roll_number=int(input("enter your roll number"))
+cgpa=float(input("enter the cgpa:"))
+percentage=cgpa*10
+roll_number=roll_number+1
+print("name of the student:",name)
+print("roll number:",roll_number)
