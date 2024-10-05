266af557199596a3b687326d0601792d104aa0ea
Liya408 <liyabn80@gmail.com>
2024-10-05 16:10:35 +0530

    Add files via upload

---
+++ operator  q3.py
@@ -0,0 +1,10 @@
+'''#author:Liya Binu
+date:5-10-2024
+program:operators'''
+a=10
+b=5
+print("sum: ",(a+b),"division: ",(a/b))
+print("is a greater than b?:",(a>b))
+print("are a and b are equal?:",(a==b))
+print("logical AND:",(a>b and b<a))
+print("logical OR:",(a>b or b>a))
