01fb510bf9b8ffb5e7c679438234263f7218ece9
Liya408 <liyabn80@gmail.com>
2024-10-05 16:10:56 +0530

    Add files via upload

---
+++ math module q4.py
@@ -0,0 +1,11 @@
+'''#author:Liya Binu
+date:5-10-2024
+program:math module'''
+import math
+number=int(input("Enter a number"))
+print("enter a number:",(number))
+print("square root of",(number),":",math.sqrt(number))
+print("Factorial of",(number),":",math.factorial (number) )
+print((number),"raised to power 2",math.pow(number,2))
+
+
