5217a59c9d75691eded669931fdf1c9b6fabdae8
Liya408 <liyabn80@gmail.com>
2024-10-05 16:06:53 +0530

    Add files via upload

---
+++ hello student.py
@@ -0,0 +1,6 @@
+'''#author:Liya Binu
+date:5-10-2024
+programe name:hellow student'''
+name=input("enter your name")
+age=input("enter your age")
+print("hello,",(name),"!""you are",(age),"years old.")
