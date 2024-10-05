381812cc8d7b60412b2706d6521f68faff6df98c
Liya408 <liyabn80@gmail.com>
2024-10-05 16:11:25 +0530

    Add files via upload

---
+++ formating stringsQ5.py
@@ -0,0 +1,4 @@
+'''#author:Liya Binu
+date:5-10-2024
+program name:formatting sample strins'''
+print("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are")
