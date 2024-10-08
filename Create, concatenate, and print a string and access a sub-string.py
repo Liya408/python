fb006df2627f27dd2990bb3572a15e8696fa8f43
Liya408 <liyabn80@gmail.com>
2024-10-08 15:37:36 +0530

    Add files via upload

---
+++ Create, concatenate, and print a string and access a sub-string.py
@@ -0,0 +1,11 @@
+"""#author:Liya Binu
+date-8-10-2024
+program-Create, concatenate, and print a string and access a sub-string from a given string"""
+
+
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+full_name=(first_name+" "+last_name)
+print(full_name)
+length=len(first_name)
+print(full_name[:length])
