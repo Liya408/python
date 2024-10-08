551ad16920f13a3bb2e3de37fed0d29c9f9e5a21
Liya408 <liyabn80@gmail.com>
2024-10-08 15:37:11 +0530

    Add files via upload

---
+++ arithemetic operations.py
@@ -0,0 +1,19 @@
+"""#authoe:Liya Binu
+date-8-10-2024
+program:arithemetic operators"""
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+num3=int(input("enter num3"))
+sum=(num1+num2)
+print("The sum of", num1 ,"and", num2 ,"is :", sum)
+difference=(num1-num2)
+print("The product of", num1, "and", num2,"is:",difference)
+product=(num1*num2)
+print("The product of", num1, "and", num2, "is:", product)
+division=float(num1/num2)
+print("The quotient when", num1,"is divided by", num2,"is:", division)
+reminder=float(num1%num2)
+print("The remainder when", num1,"is divided by", num2, "is:" ,reminder)
+div2=float(num3/2)
+multiple=(sum*div2)
+print("The result of (",num1,"+", num2,") *", num3, "/ 2 is:", multiple)
