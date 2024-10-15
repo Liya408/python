8dbba101c76aa1d21818bb98433402c843536354
Liya408 <liyabn80@gmail.com>
2024-10-15 15:02:01 +0530

    Add files via upload

---
+++ eligibility for driving license.py
@@ -0,0 +1,10 @@
+"""
+author:liya binu
+date:15-10-2024
+program:to determine the eligibility for driving license based on age
+"""
+age=int(input("enter your age"))
+if age>=16:
+    print("eligible for driving license")
+else:
+    print("not eligible for driving license")
