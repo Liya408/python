9cc455b27a9720a6ab68a4370ebdcd428e955f0b
Liya408 <liyabn80@gmail.com>
2024-10-15 16:21:53 +0530

    Add files via upload

---
+++ sum of digits.py
@@ -0,0 +1,13 @@
+"""
+author:liya binu
+date:15-10-2024
+program:sum of digits of a number
+"""
+num1=int(input("enter a number"))
+sum=0
+while num1>0:
+   r=num1%10
+   num1=num1//10
+   sum=sum+r
+print(sum)
+
