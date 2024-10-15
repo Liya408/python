7e2fd27321969de515785a07481fd789a75cf558
Liya408 <liyabn80@gmail.com>
2024-10-15 16:08:53 +0530

    Add files via upload

---
+++ odd numbers up to 'n'.py
@@ -0,0 +1,10 @@
+"""
+author:liya binu
+date:15-10-2024
+program:odd numbers up to n
+"""
+limit=int(input("enter the limit"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number,"\t",end=" ")
+    odd_number+=2
