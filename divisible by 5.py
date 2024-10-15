5459a6e5ad35ea1d508a2f9ed6802c16f528d03e
Liya408 <liyabn80@gmail.com>
2024-10-15 15:07:36 +0530

    Add files via upload

---
+++ divisible by 5.py
@@ -0,0 +1,10 @@
+"""
+author:liya binu
+date:15-10-2024
+program:To check the number is divisible by 5
+"""
+num1=int(input("enter a number"))
+if num1%5==0:
+    print(num1,"is divisible by 5")
+else:
+    print(num1,"is not divisible by 5")
