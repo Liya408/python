d2df091116311a83b76fc76add868821e24f9bb5
Liya408 <liyabn80@gmail.com>
2024-10-15 14:05:05 +0530

    Add files via upload

---
+++ if conditions.py
@@ -0,0 +1,10 @@
+"""
+atthor:liya binu
+date:15-10-2024
+program:to cheak the given number is positive or not
+"""
+number=int(input("Enter a number"))
+if number>0:
+    print("the given number",number,"is positive")
+else:
+    print("the given number"," is negative")
