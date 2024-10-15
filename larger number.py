d1360e9d640158e9928e7414b754e98beebaef71
Liya408 <liyabn80@gmail.com>
2024-10-15 14:23:02 +0530

    Add files via upload

---
+++ larger number.py
@@ -0,0 +1,11 @@
+"""
+author:liya binu
+date:15-10-2024
+program:to check which number is larger
+"""
+num1=int(input("enter an number"))
+num2=int(input("enter another number"))
+if num1>num2:
+    print(num1,"is larger than",num2)
+else:
+    print(num1,"is less than",num2)
