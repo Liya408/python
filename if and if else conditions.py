ba0adcbeacb905deb52e670a1c230618bf7baf37
Liya408 <liyabn80@gmail.com>
2024-10-15 14:12:52 +0530

    Add files via upload

---
+++ if and if else conditions.py
@@ -0,0 +1,12 @@
+"""
+author:liya binu
+date:15-10-2024
+program:to check the given number is positive or not
+"""
+number=int(input("Enter a number"))
+if number>0:
+    print("the given number",number,"is positive")
+elif number<0:
+    print("the given number"," is negative")
+else:
+    print("print the given number",number,"is zero")
