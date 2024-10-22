0905dc5c54bbd64838c0474bc7aebc7eebec3c7e
Liya408 <liyabn80@gmail.com>
2024-10-22 15:01:45 +0530

    Update celsius to fahrenheit.py

--- celsius to fahrenheit.py
+++ celsius to fahrenheit.py
@@ -3,11 +3,11 @@ date:22-10-2024
 program:to convert temperature units"""
 temp=int(input("enter the temperature:"))
 unit=(input("is it in celsius(C) or fahrenheit(F):"))
-f=((9/5)*temp)+32
-c=(5/9)*(temp-32)
 if unit=='C':
+    f=((9/5)*temp)+32
     print(temp,"C is",f,"F")
 elif unit=='F':
+    c=(5/9)*(temp-32)
     print(temp,"F is",c,"C")
 else:
     print("not valid")
