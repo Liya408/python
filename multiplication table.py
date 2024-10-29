3a22261b69a0a25309254a3248c7469dea122fce
Liya408 <liyabn80@gmail.com>
2024-10-29 16:09:11 +0530

    Update multiplication table.py

--- multiplication table.py
+++ multiplication table.py
@@ -1,3 +1,6 @@
+"""author:Liya Binu
+Date:29-10-2024
+program:To create multiplication table"""
 limit=int(input("enter a number"))
 for number in range(1,13):
-    print(limit,"*",number,"=",limit*number)
+    print(limit,"*",number,"\t=",limit*number)
