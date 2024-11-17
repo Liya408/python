37b052896947f792dd4487491bcc4a96c7f90da6
Liya408 <liyabn80@gmail.com>
2024-11-17 13:29:54 +0530

    Add files via upload

---
+++ radius of circle.py
@@ -0,0 +1,7 @@
+"""Author:Liya Binu
+Date:17-11-2024
+program to find the radius of the circle"""
+radius=float(input("Enter the radius of the circle(in cm):"))
+import math
+area=math.pi*radius**2
+print("Area of the circle is",area)
